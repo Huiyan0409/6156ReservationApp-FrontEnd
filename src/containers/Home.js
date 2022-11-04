fd3bd2478808f6d0e740f1d3733ba7806ddc7184
Huiyan0409 <nicolezhang@brandeis.edu>
2022-11-03 22:15:53 -0400

    front end initial commit

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Scratch</h1>
+                <p className="text-muted">A simple online reservation app</p>
+            </div>
+        </div>
+    );
+}
